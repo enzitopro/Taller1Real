a88e9fbfbcccf65e2d95083bc5e8f489f7b21e5b
enzitopro <ajipicantemrdayt@gmail.com>
2026-03-19 16:24:10 -0300

    add menu principal

--- src/Taller1/Taller1.java
+++ src/Taller1/Taller1.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 
 public class Taller1 {
-    public static void main(String[] args) {
         // para cargar usuarios
 	static String[] idUsuarios = new String[300];
 	static String[] passUsuarios = new String[300];
@@ -17,6 +16,41 @@ public class Taller1 {
 	static int[] regHoras = new int[300];
 	static String[] regActividades = new String[300];
 	static int totalRegistros = 0;
-    
-    }
+
+    public static void main(String[] args) {
+		cargarUsuarios();
+		cargarRegistros();
+		
+		Scanner lector = new Scanner(System.in);
+		boolean salir = false;
+		
+		while (!salir) {
+			System.out.println("1) Menu de Usuarios");
+			System.out.println("2) Menu de Analisis");
+			System.out.println("3) Menu de Salir");
+			System.out.println("> ");
+			
+			try {
+				int opcion = Integer.valueOf(lector.nextLine());
+				
+				switch (opcion) {
+				case 1:
+					menuUsuarios(lector);
+					break;
+				case 2:
+					menuAnalisis(lector);
+					break;
+				case 3:
+					salir = true;
+					System.out.println("Fin del programa");
+					break;
+				default:
+					System.out.println("Opcion inválida");
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Ingrese un número válido");
+			}
+		}
+		lector.close();
+	}
 }
