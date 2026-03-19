8d55bc00d93a765adbb5986ceb936068ef8dd366
enzitopro <ajipicantemrdayt@gmail.com>
2026-03-19 16:21:23 -0300

    add variables

--- src/Taller1/Taller1.java
+++ src/Taller1/Taller1.java
@@ -1,7 +1,22 @@
 package Taller1;
 
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 public class Taller1 {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        // para cargar usuarios
+	static String[] idUsuarios = new String[300];
+	static String[] passUsuarios = new String[300];
+	static int totalUsuarios = 	0;
+	
+	// para cargar registros
+	static String[] regUsuarios = new String[300];
+	static String[] regFechas = new String[300];
+	static int[] regHoras = new int[300];
+	static String[] regActividades = new String[300];
+	static int totalRegistros = 0;
+    
     }
 }
